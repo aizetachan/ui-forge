ce2d6d648d6c4d523f812defbba9ee2e6889ac12
aizetachan <santferal@gmail.com>
2026-02-23 13:53:14 +0100

    chore: clean up test files and update .gitignore

--- test_regex.js
+++
@@ -1,4 +0,0 @@
-const cssText = ".Button_button:hover {\n  background-color: var(--primary-hover);\n}";
-const pseudo = ":hover";
-console.log(cssText.replace(new RegExp(':' + pseudo.substring(1), 'g'), ''));
-console.log(cssText.replace(/;/g, ' !important;'));
